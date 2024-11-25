a331d85fb542f163e21a7f4c4a224043dcd3bdc5
Liveeee7219 <dlevijackson2@gmail.com>
2024-11-25 14:48:53 -0500

    Removing old stuff

---
+++ index.js
@@ -0,0 +1,32 @@
+import { css as transformStyleObject } from '@theme-ui/css'
+import { css as createClassName } from '@emotion/css'
+
+const root = document.getElementById('root')
+
+/** @type {import("@theme-ui/css").Theme} */
+const theme = {
+  colors: {
+    text: '#21',
+  },
+  fonts: {
+    mono: 'monospace',
+  },
+  space: {
+    sm: '1rem',
+    md: '2rem',
+  },
+}
+
+const styles = transformStyleObject({
+  padding: ['sm', 'md'],
+  border: ({ colors }) => `2px solid ${colors.text}`,
+  h1: {
+    fontFamily: 'mono',
+    color: 'text',
+  },
+})(theme)
+
+root.classList.add(createClassName(styles))
+root.innerHTML = `
+<h1>You can use <code>@theme-ui/css</code> in Vanilla JS!</h1>
+`
